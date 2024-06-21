9147ac7371dbb26d366d37dbb74b66756c7c6a82
Alenochka2003 <alena.filina@icloud.com>
2024-06-21 15:28:06 +0200

    font color and flex

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import CatImage from './components/CatImage';
-import styles from './components/App.module.css';
+
 
 const App = () => {
     return (
-        <div className={styles.appContainer}>
+        <div>
             <CatImage />
         </div>
     );
